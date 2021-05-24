073a06ed0ecc84acc68e13a98bd0f7c05bc9769e
QIUNAI <1773326851@qq.com>
2021-05-24 13:48:50 +0800

    Site updated: 2021-05-24 13:46:35

---
+++ js/header/index.js
@@ -0,0 +1,36 @@
+const nav = $('#navHeader');
+const navTop = nav.offset().top;
+$(window).on('scroll', function() {
+    const winTop_2 = $(window).scrollTop();
+    /*if (winTop_2 > navTop) {
+        nav.addClass('header-bg-color');
+        nav.addClass('header1');
+        nav.addClass('header2');
+    }
+    if ((winTop_2 <= navTop)) {
+        nav.removeClass('header-bg-color');
+        nav.removeClass('header1');
+        nav.removeClass('header2');
+    } */
+    var scrollFunc = function(e) {
+        e = e || window.event;
+        if (e.wheelDelta) { //第一步：先判断浏览器IE，谷歌滑轮事件    
+            if (e.wheelDelta > 0) { //当滑轮向上滚动时 
+
+                // nav.removeClass('header-bg-color');
+                nav.removeClass('header1');
+                $('header').fadeIn();
+                //nav.removeClass('header2');
+            }
+            if (e.wheelDelta < 0) { //当滑轮向下滚动时 
+
+                //nav.addClass('header-bg-color');
+                nav.addClass('header1');
+
+                $('header').fadeOut(1000);
+                //nav.addClass('header2');
+            }
+        }
+    }
+    window.onmousewheel = document.onmousewheel = scrollFunc;
+})
