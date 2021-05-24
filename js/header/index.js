b865e945505cbda611a96debdee94e9c29dd1d31
QIUNAI <1773326851@qq.com>
2021-05-24 20:38:58 +0800

    Site updated: 2021-05-24 20:36:04

--- js/header/index.js
+++ js/header/index.js
@@ -3,15 +3,15 @@ const navTop = nav.offset().top;
 $(window).on('scroll', function() {
     const winTop_2 = $(window).scrollTop();
     /*if (winTop_2 > navTop) {
-        nav.addClass('header-bg-color');
-        nav.addClass('header1');
-        nav.addClass('header2');
-    }
-    if ((winTop_2 <= navTop)) {
-        nav.removeClass('header-bg-color');
-        nav.removeClass('header1');
-        nav.removeClass('header2');
-    } */
+            //nav.addClass('header-bg-color');
+            nav.addClass('header1');
+            //nav.addClass('header2');
+        }
+        if ((winTop_2 <= navTop)) {
+            //nav.removeClass('header-bg-color');
+            nav.removeClass('header1');
+            //nav.removeClass('header2');
+        }*/
     var scrollFunc = function(e) {
         e = e || window.event;
         if (e.wheelDelta) { //第一步：先判断浏览器IE，谷歌滑轮事件    
