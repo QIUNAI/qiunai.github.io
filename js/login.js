12c5821144879e9d2c1f6aedc65b8d103e202fc3
QIUNAI <1773326851@qq.com>
2021-05-27 23:50:26 +0800

    Site updated: 2021-05-27 23:47:51

--- js/login.js
+++ js/login.js
@@ -1,11 +1,14 @@
 $(function() {
     //$('#navHeader').removeClass('header1');
     $("#bt1").click(function() {
-        if ($("#in1").val() == "qiunai" && $("#pwd").val() == "123") {
+        if ($("#in1").val() == "qiunai" && $("#pwd").val() == "20010120" || $("#in1").val() == "liang" && $("#pwd").val() == "20010808") {
             $("#div").append('<a href = "https://qiunai.github.io/mainindex/index.html"> QiunaiMonster </a>');
+
             //alert("登录成功");
             //$('#navHeader').addClass('header1');
         } else {
+            $("#in1").val("");
+            $("#pwd").val("");
             //$('#navHeader').removeClass('header1');
             alert("登录失败");
         }
